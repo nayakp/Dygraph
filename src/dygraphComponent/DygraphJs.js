25e7107d7821906e41eeb7ba035359b34443d344
Hari Prashanth Nayak Porika <hariprashanthp@gmail.com>
2017-09-11 14:02:26 +0530

    Update DygraphJs.js

--- src/dygraphComponent/DygraphJs.js
+++ src/dygraphComponent/DygraphJs.js
@@ -1,19 +1,26 @@
-import React, {Component} from 'react';
+import React, {
+  Component
+} from 'react';
 import Dygraph from 'dygraphs';
 import './DygraphCss.css';
 
 class DygraphJs extends Component {
-  constructor(props){
+  constructor(props) {
     super();
     this.state = {
-      data:props.data,
-      config:props.config
+      data: props.data,
+      config: props.config
     }
   }
   componentDidMount() {
     //renders the graph once the html markup is available in DOM 
-    this.props.config.labelsDiv = this.props.config.labelsDiv!==undefined?this.props.config.labelsDiv:this.refs.dyGraphLegend;
-    this.props.config.legend = this.props.config.legend!==undefined?this.props.config.legend:"always";
+    this.state.config.labelsDiv = this.state.config.labelsDiv !== undefined ? this.state.config.labelsDiv : this.refs.dyGraphLegend;
+    this.state.config.legend = this.state.config.legend !== undefined ? this.state.config.legend : "always";
+    this.state.config.gridLineWidth = this.state.config.gridLineWidth !== undefined ? this.state.config.gridLineWidth : 0.1;
+    this.state.config.connectSeparatedPoints = this.state.config.connectSeparatedPoints !== undefined ? this.state.config.connectSeparatedPoints : true;
+    this.state.config.axes.x.axisLabelFontSize = this.state.config.axes.x.axisLabelFontSize !== undefined ? this.state.config.axes.x.axisLabelFontSize : 12;
+    this.state.config.animatedZooms = this.state.config.animatedZooms !== undefined ? this.state.config.animatedZooms : true;
+    this.state.config.height = this.state.config.height !== undefined ? this.state.config.height : 400;
     
     new Dygraph(
       this.refs.dygraph,
@@ -22,13 +29,15 @@ class DygraphJs extends Component {
     );
   }
 
-  render () {
-    return (
-      <div className="dygraphContainer">
-        <div className="dygraph" ref="dygraph"></div>
-        <div className="dyGraphLegend" ref="dyGraphLegend"></div>
-      </div>
-      );
+  render() {
+    return ( <
+      div className = "dygraphContainer" >
+      <
+      div className = "dygraph"
+      ref = "dygraph" > < /div> <
+      div className = "dyGraphLegend"
+      ref = "dyGraphLegend" > < /div></div >
+    );
   }
 }
 
