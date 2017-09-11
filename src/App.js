02d6e59bd8722d3a56a0e1edf0baccc6fd572469
Hari Prashanth Nayak Porika <hariprashanthp@gmail.com>
2017-09-11 14:04:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,78 +1,78 @@
 import React, {Component} from 'react';
-import './App.css';
-import Dygraph from './dygraphComponent/DygraphJs';
-import GraphData from '../sample-data';
-import GraphData2 from '../sample-data2';
+import '../css/App.css';
+import Dygraph from '../components/dygraphComponent/DygraphJs';
+import GraphStore from '../store/graphStore';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    let self =this;
-    this.graphConfig =  {
+    let self = this;
+    this.title = 'Dygraph Demo';
+    this.sectionTitle = 'Pressure Transient Details';
+    this.graphConfig = this.createGraphConfig();
+    /*this.time = {};*/
+    this.graphData = GraphStore.getAllData();
+    this.graphData = this.graphData.map((i) => {
+      /*this.time[Date.parse(i[0])] = i[0];*/
+      return [Date.parse(i[0]), i[1]]
+    });
+  }
+
+  createGraphConfig() {
+    let self = this;
+    return {
       title: "Pressure Transient(s)",
       xlabel: "Time",
       ylabel: "Pressure (meters)",
-      gridLineWidth: 0.1,
       labels: [
         "Date",
         "Tampines Ave10 (Stn 40)"
       ],
-      connectSeparatedPoints: true,
       axes: {
         x: {
-          axisLabelFontSize: 9,
-          axisLabelFormatter: function(time) {
+          axisLabelFormatter: function (time) {
             let d = new Date(time);
-            return d.toTimeString().split(' ')[0]+ ':'+d.getMilliseconds();
+            return d.toTimeString().split(' ')[0] + ':' + d.getMilliseconds();
           },
-          valueFormatter: function(time) {
+          valueFormatter: function (time) {
             let d = new Date(time),
-                ms = ''+d.getMilliseconds(),
-                zeroPad = 3-ms.length;
-
-            return d.toTimeString().split(' ')[0]+ ':'+((zeroPad > 0 ? new Array(++zeroPad).join('0') : '') + ms);
+              ms = '' + d.getMilliseconds(),
+              zeroPad = 3 - ms.length;
+            return d.toTimeString().split(' ')[0] + ':' + ((zeroPad > 0 ? new Array(++zeroPad).join('0') : '') + ms);
           }
         },
-        y:{
-          valueFormatter: function(pressure) {
+        y: {
+          valueFormatter: function (pressure) {
             return parseFloat(pressure).toFixed(2);
           }
         }
       },
-      animatedZooms: true,
-      underlayCallback: function(canvas, area, g) {
+      underlayCallback: function (canvas, area, g) {
         /*http://dygraphs.com/gallery/#g/highlighted-region*/
-        var highlight_start = new Date(new Date('2016-07-15T10:24:17.273Z'));
-        var highlight_end = new Date(new Date('2016-07-15T10:24:31.762Z'));
-        var bottom_left = g.toDomCoords(highlight_start, -20);
-        var top_right = g.toDomCoords(highlight_end, +20);
-
-        var left = bottom_left[0];
-        var right = top_right[0];
+        let limits = self.getHighlightLimts(7000,11000),
+        bottom_left = g.toDomCoords(limits.start, -20),
+        top_right = g.toDomCoords(limits.end, +20),
+        left = bottom_left[0],
+        right = top_right[0];
 
         canvas.fillStyle = "#FFA000";
         canvas.fillRect(left, area.y, right - left, area.h);
-      },
-      height: 400
+      }
     }
-    this.time = {};
-    this.graphData = GraphData.map((i)=>{this.time[Date.parse(i[0])] =i[0];return [Date.parse(i[0]),i[1]]});
-    this.graphData2 = GraphData2.map((i)=>{this.time[Date.parse(i[0])] =i[0];return [Date.parse(i[0]),i[1]]});
   }
-
+  getHighlightLimts(start,end){
+    let graphData = this.graphData;
+    return {start:graphData[start][0],end:graphData[end][0]}
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Dygraph Demo</h1> 
+    return ( 
+      <div className = "App">
+        <div className = "App-header">
+          <h1>{this.title}</h1>
         </div>
-        <p className="App-intro">Pressure Transient Details</p>
-        <div className="grapMain">
-          <Dygraph config={this.graphConfig} data={this.graphData}/>
-        </div>
-        <h1>The below is another dygraph with some other data</h1>
-        <div className="grapMain">
-          <Dygraph config={this.graphConfig} data={this.graphData2}/>
+        <p className = "App-intro">{this.sectionTitle}</p>
+        <div className = "grapMain">
+          <Dygraph config = {this.graphConfig} data = {this.graphData}/>
         </div>
       </div>
     );
